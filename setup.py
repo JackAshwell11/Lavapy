fc838ea881eea6de3bb8f198f7569300c43e1f77
Aspect1103 <jack.ashwell1@gmail.com>
2021-09-27 16:09:20 +0100

    Initial upload.
    Pylink can connect to a voice channel and play a song via a youtube search query. Currently it only supports 1 node and 1 player, however, upgrades are planned.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Pylink',
+    version='0.1.0',
+    packages=['pylink'],
+    url='',
+    license='MIT',
+    author='Aspect1103',
+    author_email='jack.ashwell1@gmail.com',
+    description='A powerful and robust python library for interacting with Lavalink.'
+)
