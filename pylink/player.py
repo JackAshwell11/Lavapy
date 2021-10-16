7aa11a386e7798357a066a8da443841b85a58ad8
Aspect1103 <jack.ashwell1@gmail.com>
2021-10-16 10:46:25 +0100

    Changed license back to MIT license.
    Removed type hints from player.py __init__ variables. Can redo these later.

--- pylink/player.py
+++ pylink/player.py
@@ -1,22 +1,29 @@
 """
-Copyright (C) 2021 Aspect1103
+MIT License
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+Copyright (c) 2021-present Aspect1103
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <https://www.gnu.org/licenses/>.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
-from typing import Union, List, Dict, Any, Optional
+from typing import Union, List
 
-from discord import VoiceProtocol, VoiceChannel, Guild
+from discord import VoiceProtocol, VoiceChannel
 from discord.ext.commands import Bot, AutoShardedBot
 
 from .pool import _getNode
@@ -30,11 +37,11 @@ class Player(VoiceProtocol):
         self.bot = bot
         self.channel = channel
         self.node = _getNode()
-        self.volume: float = 100
-        self._voiceState: Dict[str, Any] = {}
-        self._track: Optional[Track] = None
-        self._connected: bool = False
-        self._paused: bool = False
+        self.volume = 100
+        self._voiceState = {}
+        self._track = None
+        self._connected = False
+        self._paused = False
         self._equalizer = Equalizer.flat()
 
     def __repr__(self) -> str:
