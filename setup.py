c21f794c11fbf6b0f0a59ced06cbc71cecb53280
Aspect1103 <jack.ashwell1@gmail.com>
2021-10-24 21:14:43 +0100

    Added classifiers in setup.py.
    Bumped version number.

--- setup.py
+++ setup.py
@@ -2,12 +2,21 @@ from setuptools import setup
 
 setup(
     name="Lavapy",
-    version="0.1.0",
+    version="0.1.2",
     packages=["lavapy"],
-    url="",
     python_requires=">=3.8",
     license="MIT",
     author="Aspect1103",
     author_email="jack.ashwell1@gmail.com",
-    description="A powerful and robust python library for interacting with Lavalink."
+    description="A powerful and robust python library for interacting with Lavalink.",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+      ]
 )
