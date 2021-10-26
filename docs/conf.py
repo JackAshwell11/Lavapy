82fe2b29faa8c8c0ae4d68de880f44a58113e920
Aspect1103 <jack.ashwell1@gmail.com>
2021-10-26 20:04:55 +0100

    Removed an extension and started testing documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ release = "0.1.2"
 # ones.
 extensions = [
     "sphinx.ext.duration",
-    "sphinx.ext.autosectionlabel"
+    "sphinx.ext.autodoc"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
--- docs/faq.rst
+++ docs/faq.rst
@@ -2,3 +2,11 @@ Frequently Asked Questions
 ==========================
 
 TO DO
+
+
+.. py:function:: lavapy.Equalizer.flat()
+
+   Returns a flat equalizer.
+
+   :return: An Equalizer object
+   :rtype: Equalizer
