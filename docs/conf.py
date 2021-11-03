b162018c7221cb3e785d8c127c8de1fe226b4470
Aspect1103 <jack.ashwell1@gmail.com>
2021-11-03 22:34:06 +0000

    test1

--- docs/conf.py
+++ docs/conf.py
@@ -60,5 +60,5 @@ intersphinx_mapping = {
 # (such as style sheets) here, relative to this directory. They are copied after
 # the builtin static files, so a file named "default.css" will overwrite the
 # builtin "default.css".
-html_theme = "furo"
+html_theme = "sphinx_rtd_theme"
 html_logo = "logo.png"
