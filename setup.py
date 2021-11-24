602b2c4119a203c5dd796213976563de38bf9771
Aspect1103 <jack.ashwell1@gmail.com>
2021-11-24 16:07:35 +0000

    Added more stuff to setup.py and reordered it.

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 import re
 from setuptools import setup
 
@@ -7,28 +8,39 @@ with open("lavapy/__init__.py") as init:
 with open("README.rst") as rdme:
     readme = rdme.read()
 
+with open("requirements.txt") as rqmnts:
+    requirements = rqmnts.read().splitlines()
+
+if os.getenv("READTHEDOCS") == "True":
+    with open("docs/requirements_rtd.txt") as rqmnts:
+        requirements.extend(rqmnts.read().splitlines())
+
 setup(
     name="Lavapy",
     version=version,
+    description="A powerful and robust Python library built from the ground up for interacting with Lavalink.",
+    long_description=readme,
     author="Aspect1103",
     author_email="jack.ashwell1@gmail.com",
     license="MIT",
     url="https://github.com/Aspect1103/Lavapy",
-    description="A powerful and robust Python library built from the ground up for interacting with Lavalink.",
-    long_description=readme,
-    packages=["lavapy"],
+    packages=["lavapy", "lavapy.ext.spotify"],
+    keywords=["lavapy", "lavalink", "discord.py"],
     python_requires=">=3.8",
+    install_requires=requirements,
+    include_package_data=True,
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Topic :: Internet",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Utilities",
+        "Typing :: Typed"
     ]
 )
